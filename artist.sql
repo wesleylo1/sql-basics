fc0f081415479db99fe0379b23c4f324f927679b
Wesley Lo <wesleylo1995@gmail.com>
2022-01-31 14:53:23 -0600

    artist.sql complete

---
+++ artist.sql
@@ -0,0 +1,15 @@
+-- 1
+INSERT INTO artist (name)
+VALUES ('MC JIN','Switchfoot','NF');
+-- 2
+SELECT * FROM artist
+ORDER BY name DESC
+LIMIT 10;
+-- 3
+SELECT * FROM artist
+ORDER BY name 
+LIMIT 5;
+-- 4
+SELECT * FROM artist
+WHERE name
+LIKE 'Black%';
