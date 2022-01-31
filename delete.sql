d64ba4f260db7203d0428b7b0ef1d1f5aca2414a
Wesley Lo <wesleylo1995@gmail.com>
2022-01-31 15:59:00 -0600

    delete.sql complete

--- delete.sql
+++ delete.sql
@@ -8,3 +8,30 @@
 -- ('Nala', 'lion', 2),
 -- ('Marie', 'cat', 1),
 -- ('Flounder', 'fish', 8);
+
+--1
+CREATE TABLE animals (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30),
+    type TEXT,
+    age INTEGER
+);
+--2
+INSERT INTO animals ( name, type, age ) 
+VALUES ('Leo', 'lion', 12),
+('Jerry', 'mouse', 4),
+('Marty', 'zebra', 10),
+('Gloria', 'hippo', 8),
+('Alex', 'lion', 9),
+('Melman', 'giraffe', 15),
+('Nala', 'lion', 2),
+('Marie', 'cat', 1),
+('Flounder', 'fish', 8);
+--3
+SELECT * FROM  animals;
+--4
+DELETE FROM animals WHERE type = 'lion';
+--5
+DELETE FROM animals WHERE LIKE 'M%';
+--6
+DELETE FROM animals WHERE age < 9;
