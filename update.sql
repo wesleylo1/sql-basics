ff6afd8e7ef284646212c362dbb67df85e827d4f
Wesley Lo <wesleylo1995@gmail.com>
2022-01-31 15:43:19 -0600

    updated.sql

---
+++ update.sql
@@ -0,0 +1,10 @@
+-- 1
+UPDATE customer SET fax = null WHERE fax is NOT NULL;
+-- 2
+UPDATE customer SET company = 'Self' WHERE company is NULL;
+-- 3
+UPDATE customer SET last_name = 'Thompson' WHERE first_name = 'Julia' AND lst_name ='BARNETT';
+-- 4
+UPDATE customer SET support_rep_id = 4 WHERE email = 'luisrojas@yahoo.cl';
+-- 5
+UPDATE track SET composer = 'The darkness around us' WHERE genre = (SELECT genre_id FROM genre WHERE name = 'Metal') AND composer is NULL;
